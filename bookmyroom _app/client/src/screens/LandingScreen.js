14a790dd5c1918ed4cda4d2595d2ab8f7775b0c6
LernToGrow <sumitkamble1252@gmail.com>
2022-12-20 00:16:06 +0530

    new changes done

---
+++ bookmyroom _app/client/src/screens/LandingScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function LandingScreen() {
+    return (
+        <div className="row landing justify-content-center">
+            <div className="col-md-8 my-auto text-center" style={{borderRight:'8px solid white'}}>
+                <h3 style={{color:"white" , fontSize:'90px'}}>BookMyRoom</h3>
+                <h1 style={{color:"white"}}> "There is only one boss. The guest....."</h1>
+                <Link to='/home'>
+                    <button className="btn landingbtn">Get Started</button>
+                </Link>
+                <Link to='/login'>
+                    <button className="btn landingbtn ml-4">Login Here</button>
+                </Link>
+            </div>
+        </div>
+    )
+};
+
+export default LandingScreen;
